d77990722a007d604e86e734918610b461b34d3f
Fred Kumi <ratti10gh@yahoo.com>
2021-01-09 01:19:41 +0000

    Added support for loading cascading lookups in report component

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { Subject } from 'rxjs';
-import { ReportService } from './report.service';
-import { takeUntil, finalize } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { BlockUI, NgBlockUI } from 'ng-block-ui'
+import { Subject } from 'rxjs'
+import { ReportService } from './report.service'
+import { takeUntil, finalize } from 'rxjs/operators'
 
 interface ReportConfig {
   title: string
@@ -18,37 +18,80 @@ interface ReportConfig {
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  styleUrls: ['./report.component.scss'],
 })
 export class ReportComponent implements OnInit, OnDestroy {
-
   reports: ReportConfig[]
   selectedReport: ReportConfig
   @BlockUI() blockUi: NgBlockUI
   unsubscribe$ = new Subject<void>()
   params: any
 
-  constructor(private reportService: ReportService) { }
+  constructor(private reportService: ReportService) {}
 
   ngOnInit() {
     this.reports = [
       {
         title: 'Subscriber List',
         query: 'reports/subscriberslist/list',
-        columns: ['code', 'name', 'phoneNumber', 'gender', 'subscriberType', 'region', 'district', 'location', 'primaryCommodity'],
+        columns: [
+          'code',
+          'name',
+          'phoneNumber',
+          'gender',
+          'subscriberType',
+          'region',
+          'district',
+          'location',
+          'primaryCommodity',
+        ],
         data: [],
         filters: [
-          { label: 'Region', type: 'select', model: 'regionId', lookupUrl: 'region/selectget', lookupStore: 'regions' },
-          { label: 'District', type: 'select', model: 'districtId', lookupUrl: 'district/selectget', lookupStore: 'districts' },
+          {
+            label: 'Region',
+            type: 'select',
+            model: 'regionId',
+            lookupUrl: 'region/selectget',
+            lookupStore: 'regions',
+            hasChild: true,
+          },
+          {
+            label: 'District',
+            type: 'select',
+            model: 'districtId',
+            lookupUrl: 'district/selectget',
+            lookupStore: 'districts',
+            hasChild: true,
+            hasParent: true,
+            parentModelName: 'regionId',
+          },
           { label: 'Location', type: 'text', model: 'location' },
-          { label: 'Commodity', type: 'select', model: 'commodityId', lookupUrl: 'commodity/selectget', lookupStore: 'commodities' },
-          { label: 'Program', type: 'select', model: 'programId', lookupUrl: 'program/selectget', lookupStore: 'programs' },
-          { label: 'Type', type: 'select', model: 'typeId', lookupUrl: 'subscribertype/selectget', lookupStore: 'subscriberTypes' }
+          {
+            label: 'Commodity',
+            type: 'select',
+            model: 'commodityId',
+            lookupUrl: 'commodity/selectget',
+            lookupStore: 'commodities',
+          },
+          {
+            label: 'Program',
+            type: 'select',
+            model: 'programId',
+            lookupUrl: 'program/selectget',
+            lookupStore: 'programs',
+          },
+          {
+            label: 'Type',
+            type: 'select',
+            model: 'typeId',
+            lookupUrl: 'subscribertype/selectget',
+            lookupStore: 'subscriberTypes',
+          },
         ],
         downloadWordUrl: 'reports/subscriberslist/word',
         downloadExcelUrl: 'reports/subscriberslist/excel',
-        downloadPdfUrl: 'reports/subscriberslist/pdf'
-      }
+        downloadPdfUrl: 'reports/subscriberslist/pdf',
+      },
     ]
   }
 
@@ -60,12 +103,13 @@ export class ReportComponent implements OnInit, OnDestroy {
   getReportData(params: any) {
     this.blockUi.start('Loading...')
     this.params = params
-    this.reportService.getReport(this.selectedReport.query, params)
+    this.reportService
+      .getReport(this.selectedReport.query, params)
       .pipe(
         takeUntil(this.unsubscribe$),
         finalize(() => this.blockUi.stop())
       )
-      .subscribe(res => {
+      .subscribe((res) => {
         if (res.success) {
           this.selectedReport.data = res.data
         }
--- src/app/shared/components/report-filter/report-filter.component.ts
+++ src/app/shared/components/report-filter/report-filter.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
-import { ReportFilterService } from './report-filter.service';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core'
+import { ReportFilterService } from './report-filter.service'
 
 @Component({
   selector: 'app-report-filter',
   templateUrl: './report-filter.component.html',
-  styleUrls: ['./report-filter.component.scss']
+  styleUrls: ['./report-filter.component.scss'],
 })
 export class ReportFilterComponent implements OnInit, OnChanges {
-
   model = { size: 50 }
   @Input() filters: any = []
   @Output() generate = new EventEmitter<any>()
   lookups: any = {}
 
-  constructor(private reportFilterService: ReportFilterService) { }
+  constructor(private reportFilterService: ReportFilterService) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   ngOnChanges(_changes: SimpleChanges): void {
     if (this.filters) {
@@ -34,18 +41,43 @@ export class ReportFilterComponent implements OnInit, OnChanges {
     this.generate.emit(this.model)
   }
 
+  onSelected(filter, value) {
+    console.log(filter, value)
+    // if (!value) {
+    //   this.filters
+    //     .filter((item: any) => item.type === 'select' && item.hasParent)
+    //     .map((item: any) => {
+    //       this.lookups[item.lookupStore] = []
+    //       this.model[item.model] = null
+    //     })
+    //   return
+    // }
+
+    if (filter.hasChild) {
+      const child = this.filters.find(
+        (obj) => obj.parentModelName === filter.model
+      )
+      const url = child.lookupUrl
+      child.lookupUrl = `${child.lookupUrl}${value}`
+      this.doLoadLookup(child)
+      child.lookupUrl = url
+    }
+  }
+
   private loadLookups(filters: []) {
     filters
-      .filter((item: any) => item.type === 'select')
+      .filter((item: any) => item.type === 'select' && !item.hasParent)
       .map((item: any) => {
-        this.reportFilterService.fetch(item.lookupUrl).subscribe(res => {
-          if (res.success) {
-            this.lookups[item.lookupStore] = {
-              label: item.label,
-              data: res.data
-            }
-          }
-        })
+        this.doLoadLookup(item)
       })
+  }
+
+  private doLoadLookup(item: any) {
+    this.reportFilterService.fetch(item.lookupUrl).subscribe((res) => {
+      this.lookups[item.lookupStore] = {
+        label: item.label,
+        data: res,
+      }
+    })
   }
 }
